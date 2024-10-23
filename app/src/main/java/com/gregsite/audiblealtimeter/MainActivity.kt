15540dc811d1f1ec3671399f3c3d4ebbbc7ab3e7
Greg <gregoirebrougher@gmail.com>
2024-10-23 15:27:52 -0700

    Created the different rows of the main column

--- app/src/main/java/com/gregsite/audiblealtimeter/MainActivity.kt
+++ app/src/main/java/com/gregsite/audiblealtimeter/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -23,31 +28,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AudibleAltimeterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Greg",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainLayout();
             }
         }
     }
 }
 
+@Preview(showBackground = false)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Red) {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(24.dp)
-        )
+fun MainLayout() {
+    Column{
+        Box(modifier = Modifier.fillMaxHeight(0.5f).fillMaxWidth().background(Color.Red)){
+
+        }
+        Row(modifier = Modifier.fillMaxHeight(0.5f).fillMaxWidth().background(Color.Green)){
+
+        }
+        Row(modifier = Modifier.fillMaxHeight().fillMaxWidth().background(Color.Blue)){
+
+        }
     }
 }
 
-@Preview(showBackground = false)
-@Composable
-fun GreetingPreview() {
-    AudibleAltimeterTheme {
-        Greeting("Greg")
-    }
-}
+//@Preview(showBackground = false)
+//@Composable
+//fun GreetingPreview() {
+//    AudibleAltimeterTheme {
+//        Greeting("Greg")
+//    }
+//}
