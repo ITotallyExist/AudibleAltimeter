a13104996dde2593f4679b11704d6fa69e125584
Greg <gregoirebrougher@gmail.com>
2024-10-25 00:45:08 -0700

    app now hides navigation bar

--- app/src/main/java/com/gregsite/audiblealtimeter/MainActivity.kt
+++ app/src/main/java/com/gregsite/audiblealtimeter/MainActivity.kt
@@ -1,34 +1,31 @@
 package com.gregsite.audiblealtimeter
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.gregsite.audiblealtimeter.ui.theme.AudibleAltimeterTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +39,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        //hide navigation bars
+        fullscreenMode(window)
+
         setContent {
             AudibleAltimeterTheme {
                 MainLayout(this);
@@ -96,6 +97,17 @@ fun convertToFt(numberInMeters: Float): Float{
 
 fun getCurrentAlt(): Int {
     return (5);
+}
+
+fun fullscreenMode(window: Window) {
+    //get a controller object for the window insets
+    val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView);
+
+    //pick the bars to hide (navigation but not notification in this case because notification isnt covering anything up)
+    windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
+
+    //specify how user unhides the bars
+    windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE;
 }
 
 @Composable
